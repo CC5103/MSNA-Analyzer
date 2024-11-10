8441a52267d220eea1cf22236109cc06dff9467a
ZHOU <z5103389750@gmail.com>
2024-11-11 03:39:12 +0900

    Rename dataImport.py to dataProcessing.py and enhance data_set class with type hints and detailed docstrings for improved clarity and usability.

--- dataProcessing.py
+++ dataProcessing.py
@@ -4,12 +4,31 @@ import numpy as np
 import math
 
 class data_set:
-    def __init__(self, ECG, BP, MSNA, fs):
+    def __init__(self, ECG: list, BP: list, MSNA: list, fs: int):
+        """読み込んだデータにファイルタをかけることやピークを検出することができる
+        
+        Args:
+            ECG (list): ECGのデータ
+            BP (list): BPのデータ
+            MSNA (list): rMSNAのデータ
+            fs (int): サンプリング周波数
+        """
         self.ECG = ECG
         self.BP = BP
         self.MSNA = MSNA
         self.fs = fs
-    def zerofilter_sci(self, n, fc, Type, data):
+    def zerofilter_sci(self, n: int, fc: int, Type: str, data: list) -> list:
+        """バターワースフィルタをかける
+        
+        Args:
+            n (int): フィルタ次数
+            fc (int): カットオフ周波数
+            Type (str): フィルタの種類
+            data (list): フィルタをかけるデータ
+        
+        Returns:
+            list: フィルタをかけたデータ
+        """
         if Type == "band":
             b, a = signal.butter(n, [fc[0]/(self.fs/2), fc[1]/(self.fs/2)], Type, analog=False)
         else:
@@ -17,9 +36,20 @@ class data_set:
         y = signal.filtfilt(b, a, data)
         return y
 
-    def read_data(self):
+    def read_data(self) -> list:
+        """データを読み込んで，フィルタをかけるやピークを検出する
+        
+        Returns:
+            list: フィルタをかけたECGデータ
+            list: フィルタをかけたBPデータ
+            list: フィルタをかけたMSNAデータ
+            list: ECGのピーク
+            list: BPのsystolicピーク
+            list: BPのdiastolicピーク
+        """
         F_ECG = self.zerofilter_sci(2, [0.3, 28], "band", self.ECG)
-        F_BP = self.zerofilter_sci(2, [0.3, 10], "band", self.BP)
+        F_BP = self.zerofilter_sci(2, 10, "low", self.BP)
+
         timeConstant = 0.1
         fc = 1/(2*math.pi*timeConstant)
         alpha = 2 * np.pi *  fc * (1/self.fs) / (2 * np.pi *  fc * (1/self.fs) + 1)
@@ -30,7 +60,7 @@ class data_set:
             F_MSNA[n] = alpha * MSNA_abs[n] + (1 - alpha) * F_MSNA[n - 1]
         F_MSNA = F_MSNA * np.exp(1j * phase)
         F_MSNA = np.real(F_MSNA)
-        
+
         # Find peaks
         peaks_ECG, _ = find_peaks(F_ECG, np.mean(F_ECG[F_ECG>0])*2.9)
         peaks_ECG_diff = np.diff(peaks_ECG)
@@ -56,3 +86,6 @@ class data_set:
             dbp_arg.append(np.argmin(F_BP[peaks_ECG[i-1]:(peaks_ECG[i-1]+(peaks_ECG[i]-peaks_ECG[i-1])//2)]) + peaks_ECG[i-1])
 
         return F_ECG, F_BP, F_MSNA, peaks_ECG, sbp_arg, dbp_arg
+    
+if __name__ == "__main__":
+    pass
